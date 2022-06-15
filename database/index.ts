2b0093558b4478847cc3dfbf4bec825409356d83
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-15 01:23:25 +0100

    refactor: write some data, add some comments and make a query

--- database/index.ts
+++ database/index.ts
@@ -3,15 +3,53 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  // Connect the client
+
   await prisma.$connect()
-  const allUsers = await prisma.user.findMany()
+
+  // Create a user an a post
+
+  // await prisma.user.create({
+  //   data: {
+  //     name: "Matt",
+  //     email: "hello@mattcook.art",
+  //     posts: {
+  //       create: {
+  //         title: "A Composite City",
+  //         body: "Planned Utopias don’t work, the composite city is a utopia.",
+  //         slug: "a-composite-city"
+  //       }
+  //     }
+  //   }
+  // })
+
+  // Add some comments to the post
+
+  // await prisma.post.update({
+  //   where: {
+  //     slug: "a-composite-city",
+  //   },
+  //   data: {
+  //     comments: {
+  //       createMany: {
+  //         data: [
+  //           { comment: "Too much planning!" },
+  //           { comment: "Cities loose their allure through planned artificial harmoniousness." }
+  //         ]
+  //       }
+  //     }
+  //   }
+  // })
+
+  const allUsers = await prisma.post.findMany({
+      include: {
+        comments: true
+      }
+    })
   console.log(allUsers)
 }
 
-
 main()
-  .catch((e) => {
+  .catch(e => {
     throw e
   })
   .finally(async () => {
