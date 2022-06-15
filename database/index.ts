749b8835fbf9a1313279da3f872e8c872bc9ecdb
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-15 01:05:05 +0100

    feat: connect to the database

--- database/index.ts
+++ database/index.ts
@@ -1,17 +1,17 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 async function main() {
   // Connect the client
   await prisma.$connect()
-  // Queries here ...
   const allUsers = await prisma.user.findMany()
   console.log(allUsers)
 }
 
+
 main()
-  .catch(e => {
+  .catch((e) => {
     throw e
   })
   .finally(async () => {
